e7e3e4855910c0e78b00f4f21bc4e1ea99ee5453
Pablo.Montalvo <pablo.montalvo.marroquin@udla.edu.ec>
2025-05-29 08:33:10 -0500

    Se maneja los archivos, para eliminar y ver los archivos.

--- PabloMExamplesLayaous/Demo.xaml.cs
+++ PabloMExamplesLayaous/Demo.xaml.cs
@@ -1,9 +1,31 @@
+using PabloMExamplesLayaous.Repositorios;
+
 namespace PabloMExamplesLayaous;
 
 public partial class Demo : ContentPage
 {
+	private ManejoArchivosRepository _repo;
 	public Demo()
 	{
+		_repo = new ManejoArchivosRepository();
 		InitializeComponent();
+		ObtenerInformacionArchivo();
 	}
+	private async Task ObtenerInformacionArchivo()
+	{
+        TextoArchivo.Text = await _repo.ObtenerInfromacionArchivo();
+    }
+
+    private async void Boton_Clicked(object sender, EventArgs e)
+    {
+		string testo = Texto1.Text;
+		Boolean guardar = await _repo.GuardarArchivo(testo);
+		//Actualiza el archivo
+        TextoArchivo.Text = await _repo.ObtenerInfromacionArchivo();
+
+        if (!guardar)
+		{
+			throw new Exception("No se pudo guardar el archivo");
+        }
+    }
 }
---
+++ PabloMExamplesLayaous/Repositorios/ManejoArchivosRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PabloMExamplesLayaous.Repositorios
+{
+    internal class ManejoArchivosRepository
+    {
+        //Crea un archivo de texto y guarda el contenido en él
+        string path = Path.Combine(FileSystem.AppDataDirectory, "/notes.txt");
+        public async Task<bool> GuardarArchivo(string texto)
+        {
+            try
+            {
+                //string path = Path.Combine(FileSystem.AppDataDirectory, "/notes.txt");
+                await File.WriteAllTextAsync(path, texto);
+
+                if (File.Exists(path))
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        //Leer el contenido del archivo de texto
+        public async Task<string> ObtenerInfromacionArchivo()
+        {
+            if (File.Exists(path))
+            {
+                string texto = await File.ReadAllTextAsync(path);
+                return texto;
+            }
+            return "No encontre nada señor no me pege porfavor";
+
+        }
+    }
+}
